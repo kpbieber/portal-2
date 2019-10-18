290efd60ca908d3012702df0ea8b727305777b44
kpbieber <kpbieber@gmail.com>
2019-10-18 10:16:41 -0500

    Adding initial files

---
+++ Classes/Portal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Classes
+{
+    public class Portal
+    {
+        public string name { get; set; }
+        public string host { get; set; }
+        public string description { get; set; }
+        public string bannerImage { get; set; } = "https://portal.limeinst.com/css/images/logo.png";
+        public string logo { get; set; } = "https://portal.limeinst.com/css/images/logo.png";
+        public string background { get; set; } = "linear-gradient(to right, #333 0%, #333 100%);";
+        public List<string> tags { get; set; }
+    }
+}
+
